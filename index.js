3046901878a3a234b40ad576e893a233e7952e61
william5 <samwilliamj333@gmail.com>
2022-09-07 17:02:54 +0530

    add

---
+++ index.js
@@ -0,0 +1,48 @@
+// number data type
+let length=20;
+console.log(length);
+// string data type
+let firstname="Sam";
+console.log(firstname);
+
+let x=20 +"Volvo";
+console.log(x);
+
+let y=10+5+"car";
+console.log(y);
+// array
+const cars = ["Saab","Volvo","BMW"];
+console.log(cars);
+// object
+const person = {
+    firstName : "Sam",
+    lastName  : "William",
+    age     : 24
+  };
+console.log(person);
+
+let name1="William";
+console.log(name1);
+// boolean
+let a= 5;
+let b = 5;
+let z = 6;
+console.log(a==b);
+console.log(a==z);
+
+// typeof
+console.log(typeof 256);
+console.log(typeof "william");
+
+
+// // not defined
+// car;
+// console.log(car);
+
+//  value is not defined
+
+let bike;
+console.log(bike);
+
+let fruits="";
+console.log(fruits);
